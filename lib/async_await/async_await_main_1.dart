4f9e42b06e8b70af0ec0363d0ac4bcfd360f8f95
Kharis Azhar <muhammad.azhar@amartha.com>
2023-02-14 18:57:44 +0700

    fix example todo async-await

--- lib/async_await/async_await_main_1.dart
+++ lib/async_await/async_await_main_1.dart
@@ -33,21 +33,21 @@ Future<List<String>> getAllProductName(
 
 /// TODO : a. Buat function, Supaya bisa asynchronous gunakan Future<TipeData>, contoh dalam ketentuan soal adalah List
 /// menerima 2 parameter yaitu List<TipeData> listData(params1), int pengali(params2)
-Future<List<TipeData>> namaFunction(params1, params2) async {
-  /// Buat Variable sementara untuk menampung hasil duplikasi List
-  List<String> result = [];
-
-  /// TODO : b. Lakukan perulangan secara asynchronous, dan tunggu hingga looping selesai gunakan await di depan delay
-  await Future.delayed(Duration(seconds: 2), () {
-    /// Looping disini
-    /// for (var i = 0; i < blabla.length; i++) {
-    /// TODO c. tiap perulangan, kalikan dengan pengali, dan simpan hasilnya ke Variable sementara yaitu Result
-    /// result.add(blabla[i] * pengali(params2) );
-    /// }
-  });
-
-  /// TODO d. return result;
-}
+// Future<List<TipeData>> namaFunction(params1, params2) async {
+//   /// Buat Variable sementara untuk menampung hasil duplikasi List
+//   List<String> result = [];
+//
+//   /// TODO : b. Lakukan perulangan secara asynchronous, dan tunggu hingga looping selesai gunakan await di depan delay
+//   await Future.delayed(Duration(seconds: 2), () {
+//     /// Looping disini
+//     /// for (var i = 0; i < blabla.length; i++) {
+//     /// TODO c. tiap perulangan, kalikan dengan pengali, dan simpan hasilnya ke Variable sementara yaitu Result
+//     /// result.add(blabla[i] * pengali(params2) );
+//     /// }
+//   });
+//
+//   /// TODO d. return result;
+// }
 
 List<String> products = [];
 
